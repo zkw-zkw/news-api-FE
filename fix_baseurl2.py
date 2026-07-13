5c4573784110b185d546adb00dcbb29d541ceaf7
zkw-zkw <2562884721@qq.com>
2026-07-13 23:09:43 +0800

    cleanup fix scripts, update UI

--- fix_baseurl2.py
+++
@@ -1,5 +0,0 @@
-﻿import sys
-c = open("D:\\Desktop\\news-api-FE\\src\\config\\api.js", "r", encoding="utf-8").read()
-c = c.replace("http://", "")
-open("D:\\Desktop\\news-api-FE\\src\\config\\api.js", "w", encoding="utf-8").write(c)
-print("Done")
